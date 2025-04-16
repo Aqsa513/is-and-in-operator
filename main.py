35a53e53676cc8b7c0de940f4729f3b580871643
aqsa <165303221+Aqsa513@users.noreply.github.com>
2025-04-17 00:13:31 +0500

    first commit

---
+++ main.py
@@ -0,0 +1,15 @@
+# 'Is' and 'In' operators in Python
+a = [1,2,3,4]
+b = a
+c = [1,2,3,4]
+
+print(a is b)  
+print (a is c)
+print (a == c)
+print(a is not c)
+print(a == b)
+
+ # 'In' operator
+my_list = [1, 2, 3, 4]
+print(2 in my_list)
+print( 7 in my_list)
